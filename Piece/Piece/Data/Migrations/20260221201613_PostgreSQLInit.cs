b084d337200f2b658e6f50b43964938e296f4a51
projectraya <rminkova007@gmail.com>
2026-02-21 22:18:32 +0200

    Add PostgreSQL migration

---
+++ Piece/Piece/Data/Migrations/20260221201613_PostgreSQLInit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Piece.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgreSQLInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
